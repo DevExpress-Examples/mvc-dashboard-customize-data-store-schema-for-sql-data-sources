9c57e1b3bdd75aae7c2b801fc8b8c865705e7eaa
Polina Tyureva <polina.tyureva@devexpress.com>
2022-08-18 17:02:02 +0400

    update vb solution

--- VB/MvcDashboard_CustomSchemaProvider/App_Start/DashboardConfig.vb
+++ VB/MvcDashboard_CustomSchemaProvider/App_Start/DashboardConfig.vb
@@ -3,6 +3,7 @@ Imports System.Web.Routing
 Imports DevExpress.DashboardWeb
 Imports DevExpress.DashboardWeb.Mvc
 Imports DevExpress.DataAccess.Sql
+Imports DevExpress.DataAccess.Web
 Imports DevExpress.Xpo.DB
 
 Namespace MvcDashboard_CustomSchemaProvider
@@ -16,73 +17,15 @@ Namespace MvcDashboard_CustomSchemaProvider
 
             Dim dashboardFileStorage As New DashboardFileStorage("~/App_Data/Dashboards")
             DashboardConfigurator.Default.SetDashboardStorage(dashboardFileStorage)
+            DashboardConfigurator.Default.SetConnectionStringsProvider(New ConfigFileConnectionStringsProvider())
 
-            DashboardConfigurator.Default.SetDBSchemaProvider(New CustomDBSchemaProvider())
+
+            ' This method assigns the database schema provider to the Web Dashboard.
+            ' Uncomment one of the following lines depending on the provider.
+            DashboardConfigurator.Default.SetDBSchemaProvider(New LimitDBSchemaProvider())
+            ' DashboardConfigurator.Default.SetDBSchemaProvider(New ManualDBSchemaProvider())
+
         End Sub
     End Class
 
-    ' Creates a new class that defines a custom data store schema by implementing the 
-    ' IDBSchemaProvider interface.
-    Friend Class CustomDBSchemaProvider
-        Implements IDBSchemaProviderEx
-
-        Private tables() As DBTable
-        Public Sub LoadColumns(ByVal connection As SqlDataConnection, ParamArray ByVal tables() As DBTable) _
-            Implements IDBSchemaProviderEx.LoadColumns
-            For Each table As DBTable In tables
-                If table.Name = "Categories" AndAlso table.Columns.Count = 0 Then
-                    Dim categoryIdColumn As DBColumn = New DBColumn With {.Name = "CategoryID", .ColumnType = DBColumnType.Int32}
-                    table.AddColumn(categoryIdColumn)
-                    Dim categoryNameColumn As DBColumn = New DBColumn With {.Name = "CategoryName", .ColumnType = DBColumnType.String}
-                    table.AddColumn(categoryNameColumn)
-                End If
-                If table.Name = "Products" AndAlso table.Columns.Count = 0 Then
-                    Dim categoryIdColumn As DBColumn = New DBColumn With {.Name = "CategoryID", .ColumnType = DBColumnType.Int32}
-                    table.AddColumn(categoryIdColumn)
-                    Dim productNameColumn As DBColumn = New DBColumn With {.Name = "ProductName", .ColumnType = DBColumnType.String}
-                    table.AddColumn(productNameColumn)
-
-                    Dim foreignKey As New DBForeignKey({categoryIdColumn}, "Categories", CustomDBSchemaProvider.CreatePrimaryKeys("CategoryID"))
-                    table.ForeignKeys.Add(foreignKey)
-                End If
-            Next table
-        End Sub
-
-        Public Shared Function CreatePrimaryKeys(ParamArray ByVal names() As String) As StringCollection
-            Dim collection As New StringCollection()
-            collection.AddRange(names)
-            Return collection
-        End Function
-
-        Public Function GetTables(ByVal connection As SqlDataConnection, ParamArray ByVal tableList() As String) As DBTable() _
-            Implements IDBSchemaProviderEx.GetTables
-            If connection.Name = "nwindConnection" Then
-                If tables IsNot Nothing Then
-                    Return tables
-                End If
-                tables = New DBTable(1) {}
-
-                Dim categoriesTable As New DBTable("Categories")
-                tables(0) = categoriesTable
-
-                Dim productsTable As New DBTable("Products")
-                tables(1) = productsTable
-            Else
-                tables = New DBTable() {}
-            End If
-            Return tables
-        End Function
-
-        Public Function GetViews(ByVal connection As SqlDataConnection, ParamArray ByVal viewList() As String) As DBTable() _
-            Implements IDBSchemaProviderEx.GetViews
-            Dim views(-1) As DBTable
-            Return views
-        End Function
-
-        Public Function GetProcedures(ByVal connection As SqlDataConnection, ParamArray ByVal procedureList() As String) As DBStoredProcedure() _
-            Implements IDBSchemaProviderEx.GetProcedures
-            Dim storedProcedures(-1) As DBStoredProcedure
-            Return storedProcedures
-        End Function
-    End Class
 End Namespace
---
+++ VB/MvcDashboard_CustomSchemaProvider/Code/LimitDBSchemaProvider.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Collections.Specialized
+Imports DevExpress.DataAccess.Sql
+Imports DevExpress.Xpo.DB
+
+Public Class LimitDBSchemaProvider
+    Implements IDBSchemaProviderEx
+
+    Private provider As DBSchemaProviderEx
+    Public Sub New()
+        Me.provider = New DBSchemaProviderEx()
+    End Sub
+
+    Public Function GetTables(ByVal connection As SqlDataConnection, ParamArray ByVal tableList() As String) As DBTable() _
+        Implements IDBSchemaProviderEx.GetTables
+        ' Returns only the tables which names start with the letter C.
+        Return provider.GetTables(connection, tableList).Where(Function(table) table.Name.StartsWith("C")).ToArray()
+    End Function
+
+    Public Function GetViews(ByVal connection As SqlDataConnection, ParamArray ByVal viewList() As String) As DBTable() _
+        Implements IDBSchemaProviderEx.GetViews
+        ' Returns only the views which names start with Sales.
+        Return provider.GetViews(connection, viewList).Where(Function(view) view.Name.StartsWith("Sales")).ToArray()
+    End Function
+
+    Public Function GetProcedures(ByVal connection As SqlDataConnection, ParamArray ByVal procedureList() As String) As DBStoredProcedure() _
+        Implements IDBSchemaProviderEx.GetProcedures
+        ' Returns only the stored procedures with zero argumnents.
+        Return provider.GetProcedures(connection, procedureList).Where(Function(storedProcedure) storedProcedure.Arguments.Count = 0).ToArray()
+    End Function
+
+    Public Sub LoadColumns(ByVal connection As SqlDataConnection, ParamArray ByVal tables() As DBTable) _
+        Implements IDBSchemaProviderEx.LoadColumns
+        ' Loads all columns in tables.
+        provider.LoadColumns(connection, tables)
+
+    End Sub
+
+End Class
